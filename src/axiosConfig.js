dc12825c385a2f2136ef6a1074128b4fa9d7b55e
creadorcos <20460108@colima.tecnm.mx>
2025-05-28 11:02:58 -0600

    axios

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,6 +1,20 @@
-// axiosConfig.js
 import axios from 'axios';
 
-axios.defaults.withCredentials = true;
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Host': window.location.hostname
+  }
+});
 
-export default axios;
+// Interceptor para manejar errores de CORS
+instance.interceptors.request.use(config => {
+  if (process.env.NODE_ENV === 'production') {
+    config.headers['Origin'] = window.location.origin;
+  }
+  return config;
+});
+
+export default instance;
